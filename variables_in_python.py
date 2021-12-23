7b492705dd256cb8235523f5886b3f1589b1ead0
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2021-12-23 21:23:40 +0530

    variables in python file

---
+++ variables_in_python.py
@@ -0,0 +1,53 @@
+age = 28
+name = "Ankit"
+weight = 58.5
+
+print(age)
+print(name)
+print(weight)
+
+hashtag = "##############################################"
+
+print(hashtag)
+
+
+price = 100
+tax = 18
+total_price = price + tax
+print(total_price)
+
+
+print(hashtag)
+
+x = 10
+y = "geek"
+z = 20
+w = z
+w = 30
+print(x, y, z, w)
+
+print(hashtag)
+
+x = 10
+print(x)
+x = "geek"
+print(x)
+
+print(hashtag)
+# Boolean type
+is_valid = True
+# Integer type
+marks = 90
+# Float type
+pi = 3.14
+# String type
+city_name = "Noida"
+
+print(is_valid)
+print(marks)
+print(pi)
+print(city_name)
+
+print("##############################################")
+
+# print(nd) nameError
