c3da4083766a0d4dff5d8a90889fb831b1e2c42e
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2021-12-23 22:47:29 +0530

    type() method in python

---
+++ type_method.py
@@ -0,0 +1,44 @@
+"""
+Basically type method is used to show
+the type or class of a particular variable 
+"""
+
+a = 10
+print(type(a))
+# <class 'int'>
+
+b = 10.5
+print(type(b))
+# <class 'float'>
+
+c = 2 + 4j
+print(type(c))
+# <class 'complex'>
+
+a = True
+print(type(a))
+# <class 'bool'>
+
+b = None
+print(type(b))
+# <class 'NoneType'>
+
+str = "gfg"
+print(type(str))
+# <class 'str'>
+
+l = [10, 20, 30]
+print(type(l))
+# <class 'list'>
+
+t = (10, 20, 30)
+print(type(t))
+# <class 'tuple'>
+
+s = {10, 20, 30, 40}
+print(type(s))
+# <class 'set'>
+
+d = {10: "gfg", 20: "ide"}
+print(type(d))
+# <class 'dict'>
